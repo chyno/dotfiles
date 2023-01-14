1bfe66f2a7eadc6b7f10137c05d68c3f5ab7953f
John Chynoweth <jchynoweth@ncarb.org>
2023-01-13 21:00:43 -0500

    change path to dot files

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # chyno_dotfiles
 https://www.webpro.nl/articles/getting-started-with-dotfiles
 
+https://docs.github.com/en/codespaces/troubleshooting/troubleshooting-personalization-for-codespaces
+
 install.sh
 ln -sv "~/.dotfiles/git/.gitconfig" ~
 
---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "adding .gitconfit"
+# symlink of git directory to my home directory
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
+
+# symlink certs folder to my home directory
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/certs ~/.certs
