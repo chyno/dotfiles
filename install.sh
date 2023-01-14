94697c3dba5c3ca61379570035fe0e72fa7833a2
John Chynoweth <jchynoweth@ncarb.org>
2023-01-13 21:15:21 -0500

    fix paths

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "adding .gitconfit"
 # symlink of git directory to my home directory
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/git/.gitconfig ~/.gitconfig
 
 # symlink certs folder to my home directory
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/certs ~/.certs
+ln -s /workspaces/.codespaces/.persistedshare/dotfiles/certs ~/localcerts
