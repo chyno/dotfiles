ad696921aa7ca925e91668cca60486e66103bf70
John Chynoweth <jchynowe1@yahoo.com>
2023-08-27 21:36:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ ln -s /workspaces/.codespaces/.persistedshare/dotfiles/git/.gitconfig ~/.gitconf
 
 # symlink certs folder to my home directory
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/certs ~/localcerts
+
+# Update package information
+sudo apt-get update
+
+# Install Fish Shell
+sudo apt-get install fish -y
