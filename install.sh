a7ba041dd8ee400a3ea9a510dde885471a7f2247
John Chynoweth <jchynowe1@yahoo.com>
2025-01-15 17:05:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ echo "adding .gitconfit"
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/git/.gitconfig ~/.gitconfig
 
 # symlink certs folder to my home directory
-ln -s /workspaces/.codespaces/.persistedshare/dotfiles/certs ~/localcerts
+# ln -s /workspaces/.codespaces/.persistedshare/dotfiles/certs ~/localcerts
 
 # Update package information
-sudo apt update
+sudo apt update -y
 
 # Install Fish Shell
-sudo apt install fish
+sudo apt install fish -y
