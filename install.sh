3dd78a29aa1c4ae92f555caa94f23fc7997abf84
John Chynoweth <jchynowe1@yahoo.com>
2025-05-11 23:24:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,20 @@ sudo apt update -y
 
 # Install Fish Shell
 sudo apt install fish -y
+
+#Install dependencies for pupeteer
+sudo apt-get update && sudo apt-get install -y \
+  libnss3 \
+  libatk1.0-0 \
+  libatk-bridge2.0-0 \
+  libcups2 \
+  libxcomposite1 \
+  libxrandr2 \
+  libxdamage1 \
+  libxkbcommon0 \
+  libgbm1 \
+  libpango-1.0-0 \
+  libasound2 \
+  libpangocairo-1.0-0 \
+  libgtk-3-0 \
+  libxshmfence1
