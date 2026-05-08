cd065b1afe87e13c2d3445041c531be96492fe9c
John Chynoweth <jchynowe1@yahoo.com>
2026-05-07 21:55:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ fi
 
 # 2. Symlink the ".squad" folder (or anything else you need) from your personal repo into ncarb-apps
 ln -sf "$PERSONAL_REPO_DIR/.squad" "$APPS_DIR/.squad"
+ln -sf "$PERSONAL_REPO_DIR/.copilot" "$APPS_DIR/.copilot"
 # For more files, just replicate the ln -sf line above as needed
 
 # Let user know
