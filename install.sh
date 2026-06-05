7e01de52206c92b395a4a6f15d9216ed1131bb80
John Chynoweth <jchynowe1@yahoo.com>
2026-06-05 12:42:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 ln -s /workspaces/.codespaces/.persistedshare/dotfiles/git/.gitconfig ~/.gitconfig
 
-# Variables
-APPS_DIR="/home/node/workspace"
-PERSONAL_REPO_DIR="/home/node/chynoweth-squad"
-
 # Update package information
 sudo apt update -y
-
-npm install -g @github/copilot
-npm install -g @bradygaster/squad-cli
